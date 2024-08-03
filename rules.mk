6209b3a974b1b3c9a59ecbfb853f5f21d0adaf3d
Kit Liu <30439030+DesyncTheThird@users.noreply.github.com>
2024-08-03 02:37:53 +0100

    Alt Layout
    Added alt layout (modified Gallium v2).

--- rules.mk
+++ rules.mk
@@ -3,11 +3,11 @@ OLED_ENABLE = yes
 OLED_DRIVER = ssd1306
 # ENCODER_ENABLE = yes
 WPM_ENABLE = yes
-# SRC += features/repeat_key.c
-# SRC += features/select_word.c
 SRC += features/achordion.c
 COMBO_ENABLE = yes
+
 DEFERRED_EXEC_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 STENO_ENABLE = yes
 STENO_PROTOCOL = geminipr
@@ -18,7 +18,7 @@ STENO_PROTOCOL = geminipr
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 
-KEY_OVERRIDE_ENABLE = yes
+
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 # AUTO_SHIFT_ENABLE = yes # Auto Shift
