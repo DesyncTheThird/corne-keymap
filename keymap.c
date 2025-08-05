a15504398babf0d73a5bd7ed9a38468465662485
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2025-08-05 11:25:14 +0100

    Minor patch
    Renamed included data files.

--- keymap.c
+++ keymap.c
@@ -3799,7 +3799,7 @@ uint16_t render_timer;
 uint16_t anim_timer = 0;
 bool show_text = true;
 
-#include "frames.inc"
+#include "frames.h"
 
 uint8_t clean_frame = 0;
 
@@ -4103,7 +4103,7 @@ void render_modifier_state(uint8_t line) {
 
 
 
-#include "menu.inc"
+#include "menu.h"
 
 #define overlay_mask ~((1 << _CONTROL_OVERLAY) | (1 << _NUMPAD) | (1 << _MOUSE))
 #define base_layer_mask ~((1 << _BASE) | (1 << _QWERTY) | (1 << _BASIC))
