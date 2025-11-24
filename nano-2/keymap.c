09eb5645244ba11b160b3c0248e4f5566e8d2ead
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2025-11-24 20:38:58 +0000

    Trackball
    Added tap dance.

--- nano-2/keymap.c
+++ nano-2/keymap.c
@@ -1,7 +1,103 @@
 #include QMK_KEYBOARD_H
+#include <math.h>
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP,
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD,
+    TD_RESET
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum {
+    NANOKEY,
+};
+
+td_state_t cur_dance(tap_dance_state_t *state);
+
+void nanokey_finished(tap_dance_state_t *state, void *user_data);
+void nanokey_reset(tap_dance_state_t *state, void *user_data);
+
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) {
+            return TD_SINGLE_TAP;
+        }
+        return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted) {
+            return TD_DOUBLE_SINGLE_TAP;
+        } else if (state->pressed) {
+            return TD_DOUBLE_HOLD;
+        }
+        return TD_DOUBLE_TAP;
+    }
+
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) {
+            return TD_TRIPLE_TAP;
+        }
+        return TD_TRIPLE_HOLD;
+    }
+
+    if (state->count == 5) {
+        return TD_RESET;
+    }
+    
+    return TD_UNKNOWN;
+}
+
+static td_tap_t tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void nanokey_finished(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = cur_dance(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP: register_code(MS_BTN1); break;
+        case TD_SINGLE_HOLD: toggle_drag_scroll(); break;
+        case TD_DOUBLE_TAP: tap_code(MS_BTN1); register_code(MS_BTN1); break;
+        case TD_DOUBLE_HOLD: tap_code(MS_BTN1); register_code(MS_BTN1); break;
+        case TD_DOUBLE_SINGLE_TAP: tap_code(MS_BTN1); register_code(MS_BTN1); break;
+        case TD_TRIPLE_TAP: register_code(MS_BTN2); break;
+        case TD_TRIPLE_HOLD: register_code(MS_BTN2); break;
+        case TD_RESET: reset_keyboard(); break;
+        default: break;
+    }
+}
+
+void nanokey_reset(tap_dance_state_t *state, void *user_data) {
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code(MS_BTN1); break;
+        case TD_SINGLE_HOLD: toggle_drag_scroll(); break;
+        case TD_DOUBLE_TAP: unregister_code(MS_BTN1); break;
+        case TD_DOUBLE_HOLD: unregister_code(MS_BTN1); break;
+        case TD_DOUBLE_SINGLE_TAP: unregister_code(MS_BTN1); break;
+        case TD_TRIPLE_TAP: unregister_code(MS_BTN2); break;
+        case TD_TRIPLE_HOLD: unregister_code(MS_BTN2); break;
+        case TD_RESET: break;
+        default: break;
+    }
+    tap_state.state = TD_NONE;
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [NANOKEY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, nanokey_finished, nanokey_reset)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( DRAG_SCROLL )
+    [0] = LAYOUT( TD(NANOKEY) )
 };
 
 static bool scroll_lock_now = false;
@@ -24,19 +120,6 @@ void housekeeping_task_user(void) {
         tap_code(KC_CAPS);
     }
 }
-
-// bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-//     if (keycode == DRAG_SCROLL) {
-//         // test code to light up 'A' when drag scroll is active
-//         if (record->event.pressed) {
-//             register_code(KC_A);
-//         } else {
-//             unregister_code(KC_A);
-//         }
-//         return false;
-//     }
-//     return true;
-// }
 
 #define MOTION_RISE_DELAY 20
 #define MOTION_FALL_DELAY 500
@@ -77,7 +160,6 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
         }
         last_resync = timer_read();
     }
-
 
     return mouse_report;
 }
